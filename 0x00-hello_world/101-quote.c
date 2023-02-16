f1b1fb1c729f038058583b8642d5665fc735952e
Ezekiel <jezekiel234@gmail.com>
2023-02-16 14:17:14 -0800

    trial for 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+syscall(SYS_write, 1, msg, sizeof(msg));
+return 0;
+}
