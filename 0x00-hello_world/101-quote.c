d4960dd0a0b5f4ab3f51727946b9dda4b9865ad9
Ezekiel <jezekiel234@gmail.com>
2023-02-16 14:19:45 -0800

    added parenthesis around return (0) and removed errant \n

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 syscall(SYS_write, 1, msg, sizeof(msg));
-return 0;
+return (0);
 }
