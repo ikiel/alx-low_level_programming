ddd4ee62b963d90b80936b79e052191a6d26ac17
Ezekiel <jezekiel234@gmail.com>
2023-02-17 09:05:53 -0800

    created 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+a = 48;
+
+while (a < 58)
+{
+putchar(a);
+if (a != 57)
+{
+putchar(',');
+putchar(' ');
+++a;
+}
+}
+
+putchar('\n');
+
+return (0);
+}
