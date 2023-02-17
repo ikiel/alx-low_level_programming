3aa24d64790a657abe1264032f8f7b08e09cc84c
Ezekiel <jezekiel234@gmail.com>
2023-02-17 08:54:44 -0800

    created 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+
+a = 48;
+while (a < 58)
+{
+putchar(a);
+++a;
+}
+
+b = 97;
+while (b < 123)
+{
+putchar(b);
+++b;
+}
+
+putchar('\n');
+
+return (0);
+}
