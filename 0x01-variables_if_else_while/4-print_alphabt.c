0ca6378ebcdecee72256ae309da5d2334ea772b7
Ezekiel <jezekiel234@gmail.com>
2023-02-17 07:50:42 -0800

    created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+a = 97;
+while (a < 123)
+{
+if (a != 113)
+{
+putchar(a);
+}
+++a;
+}
+
+putchar('\n');
+
+return (0);
+}
