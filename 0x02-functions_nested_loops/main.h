d6e10d4f9a7fda234c1865d40c7be359777e00e2
Ezekiel <jezekiel234@gmail.com>
2023-02-21 10:29:38 -0800

    removed the print_alphabet from main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,14 +6,4 @@ int _putchar(char c);
  * print_alphabet - A function that prints the alphabet in lowercase
  */
 
-void print_alphabet(void)
-{
-int i;
-for (i = 97; i <= 122; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
-
 #endif
