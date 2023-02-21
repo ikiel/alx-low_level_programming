1a976f6c6d2257dee2465dc50f2c0bc0287e5eaa
Ezekiel <jezekiel234@gmail.com>
2023-02-21 13:15:55 -0800

    converted int into char in 8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
 
 void jack_bauer(void)
 {
-	int th;
-	int h;
-	int tm;
-	int m;
+	char th;
+	char h;
+	char tm;
+	char m;
 
-	for (th = 0; th <= 2; th++)
+	for (th = '0'; th <= '2'; th++)
 	{
-		for (h = 0; h <= 4; h++)
+		for (h = '0'; h <= '4'; h++)
 		{
-			for (tm = 0; tm <= 5; tm++)
+			for (tm = '0'; tm <= '5'; tm++)
 			{
-				for (m = 0; m <= 9; m++)
+				for (m = '0'; m <= '9'; m++)
 				{
 					_putchar (th);
 					_putchar (h);
