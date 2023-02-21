94c558f45a8c153f7947beba189f5dc119ccb55b
Ezekiel <jezekiel234@gmail.com>
2023-02-21 11:32:38 -0800

    used vi instead of emacs for 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,20 +6,21 @@
  *Return: 1 if sign is +, 0 if n is zero and -1 if sign is -
  */
 
-int print_sign(int n)
+int print_sign(int c)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
-{
-_putchar('-');
-return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	
+	else if (n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
 }
