ecc33f0b9be4d151097f22643e7788f3b634772f
Ezekiel <jezekiel234@gmail.com>
2023-02-21 07:00:13 -0800

    created 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *A program thst prints the alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
