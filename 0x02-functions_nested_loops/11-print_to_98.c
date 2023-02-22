089cff0b7be754b52a5c274e19b8c230908eb70a
Ezekiel <jezekiel234@gmail.com>
2023-02-22 02:33:16 -0800

    added the increment value

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,17 +14,20 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d, ", n);
+			n++;
 		}
 	}
 	else if (n == 98)
 	{
 		printf("%d", n);
+		n++;
 	}
 	else
 	{
 		while (n <= 98)
 		{
 			printf("%d, ", n);
+			n++;
 		}
 	}
 }
