a66d7d96b11f2061df3149d257d2b52caea6307c
Ezekiel <jezekiel234@gmail.com>
2023-02-23 02:34:54 -0800

    done up to task 10 except task 5

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+
+	if (size > 0)
+	{
+		for (height = 0; height <= size; height++)
+		{
+			for (width = 0; width <= size; width++)
+			{
+			_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,14 @@ void print_square(int size)
 	int height;
 	int width;
 
+	width = size;
+
 	if (size > 0)
 	{
 		for (height = 0; height <= size; height++)
 		{
-			for (width = 0; width <= size; width++)
+			while (width--)
+			/*for (width = 0; width <= size; width++)*/
 			{
 			_putchar('#');
 			}
