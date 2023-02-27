412d2535f94cb9641fc93db15b17c4791c1009fe
Ezekiel <jezekiel234@gmail.com>
2023-02-27 15:26:21 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int k;
+	char p;
+
+	i = 0;
+	k = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		p = s[j];
+		s[k] = p;
+		k++;
+	}
+}
