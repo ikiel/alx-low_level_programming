8628a9799b5e6da63cd181ac623829b8585bb412
Ezekiel <jezekiel234@gmail.com>
2023-02-27 14:55:09 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
 /**
- * _print_rev - prints a string in reverse
+ * print_rev - prints a string in reverse
  * @s: string to print
  */
 
 void print_rev(char *s)
 {
 	int i;
+	int j;
 
 	i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
 		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
