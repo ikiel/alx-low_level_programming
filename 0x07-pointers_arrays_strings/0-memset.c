f5de7fae5988bd718aa468697b00f14d5f680566
Ezekiel <jezekiel234@gmail.com>
2023-03-06 06:41:12 -0800

    test for task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of s with the constant byte b
+ * @s: memory area
+ * @b: constant byte to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#C - 0x07-pointers_arrays_strings
