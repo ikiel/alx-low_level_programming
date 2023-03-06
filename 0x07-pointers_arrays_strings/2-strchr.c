2050df195d4b37996f0502955de27f31461defa7
Ezekiel <jezekiel234@gmail.com>
2023-03-06 08:08:00 -0800

    test for task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: first occurence of c in s, or 0 if c not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count;
+	int j;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	for (j = 0; j < count; j++)
+	{
+		if (s[j] == c)
+		{
+			return ((s + j));
+		}
+		else if (s[j] != '\0')
+		{
+			continue;
+		}
+		else if (s[j] == '\0')
+		{
+			return (0);
+		}
+	}
+}
