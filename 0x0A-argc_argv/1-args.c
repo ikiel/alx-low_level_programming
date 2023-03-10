af89a1d9f8a875f1cbef09f93487b57ee579aaa0
Ezekiel <jezekiel234@gmail.com>
2023-03-10 13:34:37 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
