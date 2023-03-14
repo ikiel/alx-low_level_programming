bc9fd4f82026522a34ff57d7d8f6b36da2ec4d19
Ezekiel <jezekiel234@gmail.com>
2023-03-14 07:27:30 -0700

    task 2 corrected empty string with double quotes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	j = 0;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	while (s1[count] != '\0')
 		count++;
 	i = count;
