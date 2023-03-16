cfe71fac50951afc48e98b0209a090ea0f99b62d
Ezekiel <jezekiel234@gmail.com>
2023-03-16 15:54:59 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,8 +12,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *mem;
-	unsigned int i, j, count;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+	for (i = 0; i <= nmemb; i++)
+	{
+		mem[i] = 0;
+	}
+}
