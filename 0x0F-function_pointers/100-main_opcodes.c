2f26cfcf6c3cf283babe1dafa64895b6bb415ce8
Ezekiel <jezekiel234@gmail.com>
2023-03-22 18:57:26 -0700

    task 4 done

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,7 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void print_opcodes(char *addr, int size);
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+}
 
 /**
  * main - prints the opcodes of its own main function
@@ -10,41 +29,21 @@ void print_opcodes(char *addr, int size);
  *
  * Return: always O
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-    int size;
+	int n;
 
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-
-    size = atoi(argv[1]);
-    if (size < 0) {
-        printf("Error\n");
-        return 2;
-    }
-
-    print_opcodes((char *)&main, size);
-    return 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
 }
-
-/**
- * print_opcodes - print the opcodes of this program
- * @addr: address of the main function
- * @size: number of bytes to print
- *
- * Return: void
- */
-
-void print_opcodes(char *addr, int size)
-{
-    int i;
-    unsigned char *p = (unsigned char *)addr;
-
-    for (i = 0; i < size; i++) {
-        printf("%02x", *(p + i));
-    }
-    printf("\n");
-}
-
