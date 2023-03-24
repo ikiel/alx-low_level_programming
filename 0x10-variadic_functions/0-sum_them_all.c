656512c6b5006d6303a6e0eae7460c0efb2b1dbc
Ezekiel <jezekiel234@gmail.com>
2023-03-23 21:49:59 -0700

    task 0 rewrote code sequence

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,19 +12,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	int sum = 0;
+	va_list ap;
 
-	if (n > 0)
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 	{
-		va_list ap;
-
-		va_start(ap, n);
-
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(ap, int);
-		}
-		va_end(ap);
-		return (sum);
+		sum += va_arg(ap, int);
 	}
-	return (0);
+	va_end(ap);
+	return (sum);
 }
