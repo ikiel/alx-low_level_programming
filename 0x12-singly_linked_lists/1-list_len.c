b234ee0258f3d2ae31866a3675fb65f422da4f56
Ezekiel <jezekiel234@gmail.com>
2023-03-31 05:13:06 -0700

    task 1 done

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * list_len - counts number of elements in a linked list
+ * @h: the list_t list
+ *
+ * Return: number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+	const list_t *ptr;
+
+	if (h == NULL)
+	{
+		printf("This is an empty list");
+		exit(0);
+	}
+
+	count = 0;
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
