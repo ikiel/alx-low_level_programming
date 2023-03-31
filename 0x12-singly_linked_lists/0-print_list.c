0eddaf7126102331cf0cb4de266278688c8e092b
Ezekiel <jezekiel234@gmail.com>
2023-03-31 04:49:19 -0700

    test for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to list_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		printf("This list is an empty list");
+		exit();
+	}
+	list_t *ptr = h;
+
+	while (ptr->next != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("%s", ptr->str);
+		ptr = ptr->next;
+		count++;
+	}
+
+	return (count);
+}
