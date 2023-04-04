ff63fcf46641614646fbe792225c5107bffefb3a
Ezekiel <jezekiel234@gmail.com>
2023-04-04 02:57:38 -0700

    task 3: done, not tested but logic is irrefutable

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node to the end of a listint_t list
+ * @head: the listint_t list
+ * @n: the data in new node
+ *
+ * Return: address of the new element, NULL if failure
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	temp = *head;
+	ptr->n = n;
+	ptr->next = NULL;
+	if (*head = NULL)
+	{
+		*head = ptr;
+		return (ptr);
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = ptr;
+	ptr->next = NULL;
+
+	return (NULL);
+}
