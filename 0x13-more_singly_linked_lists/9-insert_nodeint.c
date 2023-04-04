aaaf422b1183f58c8abf339464c866801f8d6178
Ezekiel <jezekiel234@gmail.com>
2023-04-04 12:58:03 -0700

    task 9: corrected 'idx = 0' to 'idx == 0' in if statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
 		return (NULL);
-	if (idx = 0)
+	if (idx == 0)
 	{
 		ptr->next = *head;
 		*head = ptr;
