d7a5bdfec8f96335194197819a1b7ea46575a0a0
Ezekiel <jezekiel234@gmail.com>
2023-04-04 03:03:56 -0700

    task 3: done, tested and passed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp = *head;
 	ptr->n = n;
 	ptr->next = NULL;
-	if (*head = NULL)
+	if ((*head) == NULL)
 	{
 		*head = ptr;
 		return (ptr);
