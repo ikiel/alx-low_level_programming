8968b66bcdd5cdb3d4a6963fb7b15fcf6999db36
Ezekiel <jezekiel234@gmail.com>
2023-04-04 04:24:49 -0700

    task4: done and tested

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: the listint_t list to free
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	ptr = head;
+	temp = head;
+
+	while (ptr->next != NULL)
+	{
+		temp = ptr;
+		ptr = ptr->next;
+		free(temp);
+	}
+	free (ptr);
+}
