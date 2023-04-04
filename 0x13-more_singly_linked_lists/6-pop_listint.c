b171a00ea08e7316e1a74c3956fa40d05510c34a
Ezekiel <jezekiel234@gmail.com>
2023-04-04 05:34:43 -0700

    task 6: rewritten and works well, extra pointer variables were removed and program is now lean

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,19 +9,15 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *ptr;
 	listint_t *temp;
 	int n;
 
-	ptr = *head;
-	temp = ptr->next;
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
 
-	if (head == NULL)
+	if ((*head) == NULL)
 		return (0);
-	if (ptr == NULL)
-		return (0);
-
-	n = ptr->n;
-	ptr = temp;
+	free(temp);
 	return (n);
 }
