1c67895aa7d8137fd6393ebdac26b80e33f9bf89
Ezekiel <jezekiel234@gmail.com>
2023-04-09 04:56:27 -0700

    task 0: return 0 for non-binary number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,6 +17,8 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; i < length; i++)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		if (b[i] == '1')
 			sum += 1 << (length - 1 - i);
 	}
