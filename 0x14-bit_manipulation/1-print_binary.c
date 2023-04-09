8ad2083032bce2682034e4227a31144d32918222
Ezekiel <jezekiel234@gmail.com>
2023-04-09 06:16:12 -0700

    task 1: done, betty'd, tested but still dicey

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,19 +11,29 @@ void print_binary(unsigned long int n)
 {
 	int mask, bits, i;
 
-	if (n == NULL)
+	if (n == 0)
+	{
+		_putchar('0');
 		return;
-	/* check the number of bits in the integer */
-	bits = sizeof(n) * 8;
+	}
 
-	for (i = bits - 1; i >= 0; i--)
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	/* check the number of bits in the integer */
+	bits = sizeof(n) * 1.5; 
+	bits = bits - 1;
+
+	while ((n & (1 << bits)) == 0)
+		bits--;
+
+	for (i = bits; i >= 0; i--)
 	{
 		mask = 1 << i;
-		if ((n & mask) == '1')
-			_putchar('1');
-		else if ((n & mask) == '0')
-			_putchar('0');
+		mask = ((n & mask) ? '1' : '0');
+		_putchar(mask);
 	}
-	_putchar('\n');
 }
 
