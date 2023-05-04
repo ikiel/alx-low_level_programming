4a09149dbf3a2638e5502fc45f181b07aa9665b1
Ezekiel Olojede <jezekiel234@gmail.com>
2023-05-04 12:41:43 -0700

    task 0: done but not tested

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * binary_to_uint - changes binary to an unsigned integer
+ * @b: the binary number
+ *
+ * Return: the unsigned int or 0 if failure
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		decimal = decimal << 1;
+		if (*b++ = 1)
+			decimal += 1;
+	}
+
+	return (decimal);
+}
